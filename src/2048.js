c94133571f3e0802851354ad4c4163be06ca8e2e
koziscool <kozjob@yahoo.com>
2016-10-04 07:55:28 -0500

    react works

---
+++ src/2048.js
@@ -0,0 +1,12 @@
+var Button = React.createClass({
+  
+  render: function() {
+    return (
+        <button> Go </button>
+    );
+  }
+  
+});
+
+
+ReactDOM.render( <Button />, document.getElementById("root") );
